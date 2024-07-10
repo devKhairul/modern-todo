751aaafe7b8570d7d3cab1a612eaeb079a2f4b78
Khairul Alam <dev.khairul89@gmail.com>
2024-07-10 15:45:15 -0400

    Refactored components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from "react"
-import BackgroundHeading from "./BackgroundHeading"
-import Footer from "./Footer"
-import Header from "./Header"
-import Sidebar from "./Sidebar"
-import TodoList from "./TodoList"
-import { ToastContainer } from "react-toastify"
+import { useState } from "react";
+import BackgroundHeading from "./BackgroundHeading";
+import Footer from "./Footer";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import TodoList from "./TodoList";
 
 function App() {
-
   const [todos, setTodos] = useState([]);
 
   return (
@@ -15,18 +13,16 @@ function App() {
       <BackgroundHeading />
 
       <main className="relative w-[972px] h-[636px] bg-white rounded-[8px] shadow-[0_4px_4px_rgba(0,0,0,0.08)] grid grid-cols-[7fr_4fr] grid-rows-[59px_1fr] overflow-hidden">
-        
         <Header todos={todos} />
 
         <TodoList todos={todos} setTodos={setTodos} />
-        
-        <Sidebar todos={todos} setTodos={setTodos} />        
-        
+
+        <Sidebar todos={todos} setTodos={setTodos} />
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,37 +1,47 @@
 import DeleteButton from "./DeleteButton";
 
+interface Todo {
+  id: number;
+  text: string;
+  isCompleted: boolean;
+}
 
-export default function TodoList({todos, setTodos}) {
-    
-  const handleClick = (todo) => {
+export default function TodoList({
+  todos,
+  setTodos,
+}: {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}) {
+  const handleClick = (todo: Todo) => {
     const updatedTodos = todos.map((t) =>
       t.id === todo.id ? { ...t, isCompleted: !t.isCompleted } : t
     );
     setTodos(updatedTodos);
-  }
-  
+  };
 
-	return (
-		<ul>
+  return (
+    <ul>
+      {todos.length === 0 && (
+        <li className="h-full flex justify-center items-center al font-semibold text-gray-400">
+          Add your first todo!
+        </li>
+      )}
 
-      {todos.length === 0 && <li className="h-full flex justify-center items-center al font-semibold text-gray-400">Add your first todo!</li> }
-
-			{todos.map((todo) => (
+      {todos.map((todo) => (
         <li
           key={todo.id}
           className="flex justify-between items-center px-8 h-[50px] text-[14px] cursor-pointer border-b border-black/[8%] hover:bg-[#efefef6d]"
-          onClick={ () => { handleClick(todo) } }
+          onClick={() => {
+            handleClick(todo);
+          }}
         >
-          <span 
-            className={todo.isCompleted ? 'line-through text-[#ccc]' : ''}
-            >
+          <span className={todo.isCompleted ? "line-through text-[#ccc]" : ""}>
             {todo.text}
           </span>
           <DeleteButton id={todo.id} setTodos={setTodos} />
-          
         </li>
       ))}
-		</ul>
-	);
+    </ul>
+  );
 }
- 
